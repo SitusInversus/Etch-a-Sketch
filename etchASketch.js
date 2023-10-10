e609b7c6b3237b051d1448d38fb550904c2c500c
Christian on Mac <spiegeleulein@gmail.com>
2023-10-10 14:22:15 +0200

    Migrate Attributes FROM JS TO CSS

--- etchASketch.js
+++ etchASketch.js
@@ -6,11 +6,7 @@ let sizeGridElement = sizeGrid/columnLength;
 
 
 const body = document.querySelector("body")
-body.style.display = "flex";
-body.style.justifyContent = "center";
-body.style.backgroundColor = "grey";
-body.style.border = "1px solid red";
-body.style.flex = "stretch";
+
 
 const containerGrid = document.createElement("div");
 body.appendChild(containerGrid);
@@ -18,11 +14,6 @@ body.appendChild(containerGrid);
 containerGrid.classList.add("containerGrid");
 containerGrid.style.width = `${sizeGrid}px`;
 containerGrid.style.height = `${sizeGrid}px`;
-containerGrid.style.display = "flex";
-containerGrid.style.backgroundColor = "#7e8b003d";
-containerGrid.style.boxSizing = "border-box";
-containerGrid.style.flexWrap = "wrap";
-containerGrid.style.alignSelf = "center";
 
 for (let i = 0; i<numberGridElements; i++) {
     const squareDivLoop = document.createElement("div");
@@ -35,9 +26,5 @@ const squareDivs = document.querySelectorAll(".squareDiv");
 squareDivs.forEach(squareDiv => {
     squareDiv.style.width = `${sizeGridElement}px`;
     squareDiv.style.height = `${sizeGridElement}px`;
-    squareDiv.style.backgroundColor = "white";
-    squareDiv.style.backgroundColor = "red";
-    squareDiv.style.boxSizing = "border-box";
-    
 });
 
